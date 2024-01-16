a224b6397d1a5be593350fbe979e0c30c84cc06f
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-01-16 16:43:43 +0000

    change error handling for post comment

--- api/controllers/prepNewComment.js
+++ api/controllers/prepNewComment.js
@@ -4,10 +4,8 @@ const prepNewComment = async (req, res, next) => {
   try {
     const { article_id } = req.params;
     const { body } = req;
-    !Number(article_id) ? res.status(400).send('Bad Request') : false;
     const comment = await postNewComment(article_id, body);
-    comment.status === 400 ? res.status(400).send('Bad Request') : false;
-    !comment.hasOwnProperty('status') ? res.status(201).send({ comment }) : res.status(404).send(comment.msg)
+    !comment.hasOwnProperty('status') ? res.status(201).send({ comment }) : res.status(comment.status).send(comment.msg)
   } catch (err) { next(err) }
 }
 
--- api/models/postNewComment.js
+++ api/models/postNewComment.js
@@ -1,25 +1,20 @@
 const db = require("../../db/connection");
 
 const postNewComment = async (article_id, commentData) => {
-  const { username, body } = commentData;
-  const { rows: users } = await db.query("SELECT username FROM users");
-  const userCheck = users.some((user) => user.username === username);
-  const { rows: articles } = await db.query("SELECT * FROM articles WHERE article_id = $1", [ article_id ]);
-  if (!username || !body) {
-    return { status: 400, msg: "Bad Request"}
-  }
-  else if (!userCheck || articles.length === 0) {
-    return { status: 404, msg: "Not found" };
-  } else {
+  try {
+    const { username, body } = commentData;
     const { rows: comment } = await db.query(
-    `
-    INSERT INTO comments (body, author, article_id)
-    VALUES ($1, $2, $3)
-    RETURNING *
-    `,
+      `
+      INSERT INTO comments (body, author, article_id)
+      VALUES ($1, $2, $3)
+      RETURNING *
+      `,
       [body, username, article_id]
     );
     return comment[0];
+  } catch (err) {
+    if (err.constraint === undefined) return { status: 400, msg: 'Bad Request'}
+    else return { status: 404, msg: 'Article not found' }
   }
 };
 
